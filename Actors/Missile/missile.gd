4969d590081109bc462a882a1a24a6ed2f6444d1
Max Carvajal <jcarvajal288@gmail.com>
2026-01-12 21:40:12 -0600

    Missile moves towards player

--- Actors/Missile/missile.gd
+++ Actors/Missile/missile.gd
@@ -2,8 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+const HORIZONTAL_ACCEL = 20.0
 
 
-func _physics_process(_delta: float) -> void:
+func _ready() -> void:
 	velocity = Vector2(0, SPEED)
+
+
+func _physics_process(delta: float) -> void:
+	var acceleration = HORIZONTAL_ACCEL * delta
+	if Global.player.global_position.x < global_position.x:
+		acceleration *= -1
+	velocity.x += acceleration
 	move_and_slide()
