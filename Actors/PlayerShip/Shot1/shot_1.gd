2f3610ea661a65b7ada9ea7ea08321447cc98138
Max Carvajal <jcarvajal288@gmail.com>
2026-01-14 06:33:52 -0600

    Shot usually is removed on impact

--- Actors/PlayerShip/Shot1/shot_1.gd
+++ Actors/PlayerShip/Shot1/shot_1.gd
@@ -5,8 +5,13 @@ const SPEED = 300.0
 
 func _ready() -> void:
 	z_index = Global.RenderOrder.SHOT
+	body_entered.connect(_on_hit)
 
 
 func _physics_process(delta: float) -> void:
 	var displacement = SPEED * Vector2.UP * delta	
 	global_position += displacement
+
+
+func _on_hit(_body: Node2D) -> void:
+	queue_free()
