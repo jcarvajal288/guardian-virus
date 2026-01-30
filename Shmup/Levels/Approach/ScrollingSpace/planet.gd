f5f4d43ee7d3a650cc9033a3bad65d2e02dedaee
Max Carvajal <jcarvajal288@gmail.com>
2026-01-29 18:37:50 -0600

    Planet scrolls pixel by pixel

--- Shmup/Levels/Approach/ScrollingSpace/planet.gd
+++ Shmup/Levels/Approach/ScrollingSpace/planet.gd
@@ -1,14 +1,20 @@
-extends Parallax2D
+extends Sprite2D
 
-const time_to_complete = 60.0
+const TIME_TO_COMPLETE = 60.0
+
+var time_to_move_a_pixel = 0.0
+
+var elapsed_time = 0.0
 
 
-func _init() -> void:
-	autoscroll = Vector2(0, repeat_size.y / 60.0)
-	print(autoscroll)
+func _ready() -> void:
+	var distance = 144
+	time_to_move_a_pixel = TIME_TO_COMPLETE / distance	
+	print(time_to_move_a_pixel)
 
 
-func _process(_delta: float) -> void:
-	if scroll_offset.y >= 143:
-		autoscroll = Vector2.ZERO
-		repeat_size = Vector2.ZERO
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	if global_position.y <= 72 and elapsed_time > time_to_move_a_pixel:
+		global_position.y += 1
+		elapsed_time = 0.0
