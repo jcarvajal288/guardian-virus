0a4dc49e7a41a81eeae8db7350cb660cb336756f
Max Carvajal <jcarvajal288@gmail.com>
2026-02-07 22:16:10 -0600

    Stop multiple simultaneous hits all registering at once

--- Common/Bullets/bullet.gd
+++ Common/Bullets/bullet.gd
@@ -20,3 +20,4 @@ func _physics_process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area is Hurtbox:
 		area.register_hit(damage)
+		queue_free()
--- Shmup/Actors/PlayerShip/player_ship.gd
+++ Shmup/Actors/PlayerShip/player_ship.gd
@@ -66,20 +66,20 @@ func animate() -> void:
 
 
 func _on_hit(damage: float) -> void:
-	Sounds.play_sound.emit(Sounds.SoundEffect.PLAYER_SHIELD, global_position)
-	$Health._on_hit(damage)
-	Global.player_health_changed.emit($Health.current_health, $Health.max_health)
-
-	$Shield.visible = true
 	$Hurtbox.set_deferred("monitoring", false)
 	$Hurtbox.set_deferred("monitorable", false)
+	$Shield.visible = true
 	$Sprite2D.modulate.a = 0.5
+
+	$Health._on_hit(damage)
+	Global.player_health_changed.emit($Health.current_health, $Health.max_health)
+	Sounds.play_sound.emit(Sounds.SoundEffect.PLAYER_SHIELD, global_position)
 
 	await get_tree().create_timer(GameStats.PLAYER_HIT_INVULN_IN_SECS).timeout
 
-	$Shield.visible = false
 	$Hurtbox.set_deferred("monitoring", true)
 	$Hurtbox.set_deferred("monitorable", true)
+	$Shield.visible = false
 	$Sprite2D.modulate.a = 1.0
 
 
