8df49ed1994eab6410b05497ccb58d625ec7b79e
Max Carvajal <jcarvajal288@gmail.com>
2026-02-07 22:57:04 -0600

    Give energy bullets a dissipate animation

--- Common/Bullets/Energy/energy_bullet.gd
+++ Common/Bullets/Energy/energy_bullet.gd
@@ -7,8 +7,9 @@ const LIFETIME = 6.0
 func _ready() -> void:
 	speed = 40
 	direction = Global.TOWARDS_PLAYER
-	$LifetimeTimer.timeout.connect(queue_free)
+	$LifetimeTimer.timeout.connect(dissipate)
 	$LifetimeTimer.start(LIFETIME)
+	$AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
 	super()
 
 
@@ -17,3 +18,13 @@ func _physics_process(delta: float) -> void:
 	var turn = direction.angle_to(direction_to_player) / TURN_RATE * delta
 	direction = direction.rotated(turn)
 	super(delta)
+
+
+func dissipate() -> void:
+	$AnimatedSprite2D.play("dissipate")
+	$CollisionShape2D.disabled = true
+
+
+func _on_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == 'dissipate':
+		queue_free()
